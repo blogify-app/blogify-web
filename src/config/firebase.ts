a256d68bb4efc00c15721d70506fbc3a3d70d419
Yume <hei.hajatiana@gmail.com>
2024-01-16 23:30:14 +0300

    fix: get provided config directly

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,24 +1,15 @@
-import {__PROD__} from "@/config/env.ts";
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 
-function getEnvironmentFirebaseConfig(env: string) {
-  return {
-    apiKey: process.env[`FIREBASE_API_KEY_${env}`],
-    authDomain: process.env[`FIREBASE_AUTH_DOMAIN_${env}`],
-    projectId: process.env[`FIREBASE_PROJECT_ID_${env}`],
-    storageBucket: process.env[`FIREBASE_STORAGE_BUCKET_${env}`],
-    messagingSenderId: process.env[`FIREBASE_MESSAGING_SENDERID_${env}`],
-    appId: process.env[`FIREBASE_APP_ID_${env}`],
-    measurementId: process.env[`FIREBASE_MEASUREMENT_ID_${env}`],
-  };
-}
-
-const firebaseConfig = getEnvironmentFirebaseConfig(
-  __PROD__ ? "PROD" : "PREPROD"
-);
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-console.log("app_id", app.options.appId);
