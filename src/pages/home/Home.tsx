32be0c77fb19a779e65b8501b0bb5ed8ad6cf0ed
Yume <hei.hajatiana@gmail.com>
2024-01-16 10:23:51 +0300

    fix(test): cy.getByTestid

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,8 @@ import {FC} from "react";
 
 export const Home: FC = () => (
   <div>
-    <h1 className="text-5xl">blogify-web works</h1>
+    <h1 className="text-5xl" data-testid="typo0">
+      blogify-web works
+    </h1>
   </div>
 );
