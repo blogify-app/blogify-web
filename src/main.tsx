b3c8214239388594d004906baf0ed7a285e85196
Yumii <hei.hajatiana@gmail.com>
2024-01-17 21:52:56 +0300

    chore: setup firebase auth #BLOG-20
    * build(dep): add firebase sdk
    
    * chore: configure firebase connector
    
    * fix: get provided config directly
    
    * chore: set up AuthProvider
    
    * chore(fix): env template
    
    * chore: setup vite load_env
    
    * feat: Auth skeleton
    
    * chore: persistent auuth_store
    
    * refactor: Home => HomePage
    
    * chore: protected route
    
    * refactor: imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
-import {Home} from "./pages/home";
+import {HomePage} from "@/pages/home";
+import {LoginPage} from "@/pages/auth";
+import {Authenticated} from "@/features/auth";
+import {AuthProvider} from "@/services/auth_provider.ts";
 import "./index.css";
 
+// eslint-disable-next-line react-refresh/only-export-components
 const ROUTER = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/protected",
+    element: (
+      <Authenticated>
+        <h1>Connected</h1>
+        <button
+          onClick={() => {
+            void AuthProvider.logOut().then(() => {
+              window.location.reload();
+            });
+          }}
+        >
+          logout
+        </button>
+      </Authenticated>
+    ),
   },
 ]);
 
