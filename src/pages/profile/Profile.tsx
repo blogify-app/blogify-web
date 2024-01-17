4c64d18df25417a29662f386f16e3e4eafaa3116
NyH1409 <hei.nyhasina@gmail.com>
2024-01-17 20:39:31 +0300

    chore: basic common button

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 import profilepic from "../../assets/profile.jpg";
+import { Button } from "@/common/button";
 
 export const Profile : FC = ()=>{
     return(
-        <div>
+        <div className="bg-thumbnail">
             <div className="flex items-center p-12">
                 <div className="px-12">
                     <img className="w-40 h-40 rounded-[100%]" src={profilepic} alt="user profile" />
@@ -12,10 +13,20 @@ export const Profile : FC = ()=>{
                     <h1 className="font-bold text-3xl">Ny Hasina VAGNO</h1>
                     <p className="text-sm text-light">@hasnyvagno</p>
                     <p className="text-sm text-light">En savoir plus sur cette chaine</p>
+                    <div className="flex justify-between">
+                        <Button classNameAttr="text-xs bg-gray-800 hover:bg-gray-600 my-3 text-sm text-white py-2 px-4 rounded-[25px]"
+                        label="Personnaliser cette chaine"
+                        ></Button>
+                        <Button classNameAttr="text-xs bg-gray-800 hover:bg-gray-600 m-3 text-sm text-white py-2 px-4 rounded-[25px]"
+                        label="Gerer les contenus"
+                        ></Button>
+                    </div>
                 </div>
             </div>
             <hr />
-            <div></div>
+            <div>
+                
+            </div>
         </div>
     )
 }
