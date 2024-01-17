f63e09bea8b73a7cc03e5b67eef2219b5f8543cd
NyH1409 <hei.nyhasina@gmail.com>
2024-01-17 20:39:31 +0300

    chore: basic common button

---
+++ src/common/button/Button.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from "react";
+
+type ButtonProps = {
+    label : string,
+    children : ReactNode,
+    classNameAttr : string
+}
+
+export const Button : FC<ButtonProps> = ({ children, classNameAttr, label })=>{
+    return (
+        <button className={classNameAttr}>
+            {children}
+            <span>{label}</span>
+        </button>
+    )
+}
