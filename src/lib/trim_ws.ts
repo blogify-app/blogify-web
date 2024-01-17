73d6be063985cedb743f13f1f6c947bb7c7e6d03
Yumii <hei.hajatiana@gmail.com>
2024-01-17 23:23:16 +0300

    feat: add markdown reader #BLOG-21 (#2)
    * build(dep): rehype & remark & unified
    
    * chore: configure sanitization for vuln-free md-to-html and prettify
    
    * feat: add Reader and LazyReader component
    
    * fix: md styling
    
    * test: sanitization & unsafe Reader vulnerability
    
    * chore: fix unbound generics in cy comand
    
    * refactor: LazyReader -> Reader, Reader -> UnsafeReader
    
    * chore(shadcn-ui): gen button cmp
    
    * feat: beautify temporary home page

---
+++ src/lib/trim_ws.ts
@@ -0,0 +1,6 @@
+/**
+ * Removes whitespaces from the given string
+ */
+export const trimWs = (s: string) => {
+  return s.replace(/\s+/, "");
+};
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,53 @@
 import {FC} from "react";
 import {useAuthStore} from "@/features/auth";
+import {Reader} from "@/features/wisiwig";
+import {Button} from "@/components/shadcn-ui/button";
+import {AuthProvider} from "@/services/auth_provider";
+import {NavLink} from "react-router-dom";
 
 export const HomePage: FC = () => {
   const auth = useAuthStore();
+
+  // NOTE: @example
+  const logout = async () => {
+    try {
+      await AuthProvider.logOut();
+      auth.setUser(null);
+    } catch (e) {
+      /* TODO: handle err */
+    }
+  };
+
   return (
     <div>
-      <h1 className="text-5xl" data-testid="typo0">
-        blogify-web works
-      </h1>
-      {auth.user ? <div>username: {auth.user}</div> : null}
+      <div>
+        <Reader className="w-2/3">
+          {[
+            "# Blogify web",
+            "A blogging app for students.",
+            "## Why ?",
+            "* It is **PRETTY**",
+            "* ... Why not?\n",
+            "```js",
+            'console.log("C\'mon ... Why not??")',
+            "```",
+          ].join("\n")}
+        </Reader>
+      </div>
+
+      <div className="px-4">
+        <Reader className="px-0">
+          {[
+            "## Connected user",
+            auth.user ? `**${auth.user}**` : "_None_",
+          ].join("\n")}
+        </Reader>
+
+        <Button onClick={logout}>Logout</Button>
+        <NavLink to="/login">
+          <Button variant="link">Login</Button>
+        </NavLink>
+      </div>
     </div>
   );
 };
