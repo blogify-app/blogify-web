c13ab6c5a4cb26c472498f4c1f2983f01f556800
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-01-20 11:07:32 +0300

    fix: build failed when adding background image

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,2 +1,2 @@
 export * from "./store";
-export * from "./components/signin";
+export * from "./components";
--- src/images/image.tsx
+++
@@ -1,5 +0,0 @@
-const IMAGES = {
-  signinBackground: new URL("./signin-img.png", import.meta.url).href,
-};
-
-export default IMAGES;
