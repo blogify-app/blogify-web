8691dc03d6d659b4651191b5361e025a94e71339
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-01-21 17:36:04 +0300

    refactor: post creation UI

--- src/features/post/components/PostCreation.tsx
+++ src/features/post/components/PostCreation.tsx
@@ -39,45 +39,46 @@ export const PostCreation: FC = () => {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="w-96 rounded bg-white p-8 shadow-lg">
+    <div className="flex h-screen w-screen items-center justify-center bg-white">
+      <div className="h-4/5 w-full rounded bg-white p-8  md:w-2/3">
         <h2 className="mb-4 text-2xl font-semibold">Créer un Post</h2>
-        <div className="mb-4">
-          <Select>
-            <SelectTrigger className="w-full" {...register("category")}>
-              <SelectValue placeholder="Sélectionner une catégorie" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="est">Romantique</SelectItem>
-                <SelectItem value="cst">Science</SelectItem>
-                <SelectItem value="mst">Technologie</SelectItem>
-                <SelectItem value="pst">Langue</SelectItem>
-                <SelectItem value="akst">Voyage</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          {errors.category && (
-            <p className="text-red-300">{errors.category.message}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <Textarea
-            placeholder="Que voulez-vous partager ?"
-            className="w-full"
-            {...register("content")}
-          />
-          {errors.content && (
-            <p className="text-red-300">{errors.content.message}</p>
-          )}
-        </div>
-        <Button
-          variant="outline"
-          className="w-full"
-          onClick={handleSubmit(onSubmit)}
-        >
-          Publier
-        </Button>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div className="mb-4 flex items-center">
+            <Select {...register("category")} defaultValue="">
+              <SelectTrigger className="mx-1/4 w-1/4">
+                <SelectValue placeholder="Sélectionner une catégorie" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="est">Romantique</SelectItem>
+                  <SelectItem value="cst">Science</SelectItem>
+                  <SelectItem value="mst">Technologie</SelectItem>
+                  <SelectItem value="pst">Langue</SelectItem>
+                  <SelectItem value="akst">Voyage</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            {errors.category && (
+              <p className="text-red-300">{errors.category.message}</p>
+            )}
+            <Button
+              type="submit"
+              className="mx-4 w-1/6 bg-green-600 hover:bg-gray-500"
+            >
+              Publier
+            </Button>
+          </div>
+          <div className="mb-4">
+            <Textarea
+              placeholder="Que voulez-vous partager ?"
+              {...register("content")}
+              className="h-48 w-full resize-none border-0 text-xl font-semibold text-gray-600 focus:ring-0 focus:!ring-transparent focus-visible:ring-0"
+            />
+            {errors.content && (
+              <p className="text-red-300">{errors.content.message}</p>
+            )}
+          </div>
+        </form>
       </div>
     </div>
   );
