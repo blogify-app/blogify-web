062951caaeaab0bcba02a6c441a2f45f19343219
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-01-21 08:48:48 +0300

    feat: implement post creation minimal UI

--- src/pages/post/PostCreationPage.tsx
+++ src/pages/post/PostCreationPage.tsx
@@ -1,13 +1,45 @@
 import {FC} from "react";
 import {Button} from "@/components/shadcn-ui/button";
 import {Textarea} from "@/components/shadcn-ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/shadcn-ui/select";
 
 export const PostCreationPage: FC = () => {
   return (
-    <div>
-      <div className="grid w-80 gap-2">
-        <Textarea placeholder="Que voulez-vous partagez ?" />
-        <Button variant={"outline"}>Publier</Button>
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="w-96 rounded bg-white p-8 shadow-lg">
+        <h2 className="mb-4 text-2xl font-semibold">Créer un Post</h2>
+        <div className="mb-4">
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Sélectionner une catégorie" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="est">Romantique</SelectItem>
+                <SelectItem value="cst">Science</SelectItem>
+                <SelectItem value="mst">Technologie</SelectItem>
+                <SelectItem value="pst">Langue</SelectItem>
+                <SelectItem value="akst">Voyage</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="mb-4">
+          <Textarea
+            placeholder="Que voulez-vous partager ?"
+            className="w-full"
+          />
+        </div>
+        <Button variant="outline" className="w-full">
+          Publier
+        </Button>
       </div>
     </div>
   );
