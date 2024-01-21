7e8c4383675c022b4ef956a3f94a44be9818a1c4
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-01-21 18:03:06 +0300

    chore: add title attribut

--- src/features/post/components/PostCreation.tsx
+++ src/features/post/components/PostCreation.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import {Button} from "@/components/shadcn-ui/button";
 import {Textarea} from "@/components/shadcn-ui/textarea";
+import {Input} from "@/components/shadcn-ui/input";
 import {useParams} from "react-router-dom";
 import {useForm} from "react-hook-form";
 import {
@@ -34,6 +35,7 @@ export const PostCreation: FC = () => {
   const onSubmit = (data: PostData) => {
     console.log({
       id: postId,
+      title: data.title,
       category: data.category,
       content: data.content,
     });
@@ -41,21 +43,23 @@ export const PostCreation: FC = () => {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-white">
-      <div className="h-4/5 w-full rounded bg-white p-8  md:w-2/3">
-        <h2 className="mb-4 text-2xl font-semibold">Créer un Post</h2>
+      <div className="h-4/5 w-full rounded bg-white p-8  shadow-lg md:w-2/3">
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div className="mb-4 flex items-center">
+          <div className="mb-8 flex">
+            <h2 className="mx-4 mb-4 flex text-2xl font-semibold">
+              Nouveau post
+            </h2>
             <Select {...register("category")} defaultValue="">
               <SelectTrigger className="mx-1/4 w-1/4">
                 <SelectValue placeholder="Sélectionner une catégorie" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectItem value="est">Romantique</SelectItem>
-                  <SelectItem value="cst">Science</SelectItem>
-                  <SelectItem value="mst">Technologie</SelectItem>
-                  <SelectItem value="pst">Langue</SelectItem>
-                  <SelectItem value="akst">Voyage</SelectItem>
+                  <SelectItem value="rom">Romantique</SelectItem>
+                  <SelectItem value="sci">Science</SelectItem>
+                  <SelectItem value="tech">Technologie</SelectItem>
+                  <SelectItem value="lan">Langue</SelectItem>
+                  <SelectItem value="voy">Voyage</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
@@ -69,11 +73,19 @@ export const PostCreation: FC = () => {
               Publier
             </Button>
           </div>
-          <div className="mb-4">
+          <div className="mx-4 my-4 flex w-3/5 items-center border-b border-gray-300 py-2">
+            <Input
+              type="text"
+              placeholder="Titre"
+              {...register("title")}
+              className="border-0 text-xl text-gray-400 focus:ring-0 focus:!ring-transparent focus-visible:ring-0"
+            />
+          </div>
+          <div className="mx-4 mb-4">
             <Textarea
               placeholder="Que voulez-vous partager ?"
               {...register("content")}
-              className="h-48 w-full resize-none border-0 text-xl font-semibold text-gray-600 focus:ring-0 focus:!ring-transparent focus-visible:ring-0"
+              className="h-48 w-full resize-none border-0 text-xl font-semibold text-gray-400 focus:ring-0 focus:!ring-transparent focus-visible:ring-0"
             />
             {errors.content && (
               <p className="text-red-300">{errors.content.message}</p>
--- src/features/post/schema.ts
+++ src/features/post/schema.ts
@@ -1,6 +1,7 @@
 import * as z from "zod";
 
 export const post = z.object({
+  title: z.string().min(1, {message: "Le titre du post ne peut pas être vide"}),
   category: z.string().min(1, {message: "Veuillez sélectionner une catégorie"}),
   content: z
     .string()
