5b81eb7fc2221e77d4b75e933cec667e336bc08c
Yume <hei.hajatiana@gmail.com>
2024-01-22 23:02:52 +0300

    refactor(layout): Navbar

---
+++ src/__tests__/layout/NavBar.cy.tsx
@@ -0,0 +1,22 @@
+import {NavBar} from "@/layout";
+import {BrowserRouter} from "react-router-dom";
+
+describe("Navbar", () => {
+  it("should should be be visible", () => {
+    cy.mount(
+      // react-router-dom components require Navigation Context
+      // BrowserRouter does provide one so render "navbar" under BrowserRouter
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    );
+
+    cy.getByTestid("Navbar").should("be.visible");
+
+    cy.get("a").contains("BLOGIFY");
+
+    cy.contains("Home");
+    cy.contains("Profile");
+    cy.contains("About");
+  });
+});
