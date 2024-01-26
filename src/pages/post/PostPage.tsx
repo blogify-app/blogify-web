44e056d437254b62bab998c0ce825c4010936a81
NyAndoMayah <hei.mayah.3@gmail.com>
2024-01-26 05:53:43 +0300

    chore: use PostProvider in PostPage

--- src/pages/post/PostPage.tsx
+++ src/pages/post/PostPage.tsx
@@ -1,7 +1,18 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {Post} from "@/features/post";
-import {post} from "./data/posts";
+import {DEFAULT_QUERY, PostProvider} from "@/services/api";
+import {Post as PostType} from "@/services/api/gen";
+import {useParams} from "react-router-dom";
 
 export const PostPage: FC = () => {
+  const [post, setPost] = useState<PostType>({});
+
+  const params = useParams();
+  const id = params?.id!;
+
+  useEffect(() => {
+    PostProvider.getById(id).then((post) => setPost(post));
+  }, []);
+
   return <Post post={post} />;
 };
