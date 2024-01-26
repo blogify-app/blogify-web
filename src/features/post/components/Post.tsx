51ac65c5ff0675e8d6e25e23c85ba9388adecfca
NyAndoMayah <hei.mayah.3@gmail.com>
2024-01-26 06:01:37 +0300

    chore: use UserProvider instead of use static data

--- src/features/post/components/Post.tsx
+++ src/features/post/components/Post.tsx
@@ -1,11 +1,11 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {Icon} from "@iconify/react";
 import {Badge} from "@/components/shadcn-ui/badge";
 import {Layout} from "@/layout";
 import {calculateReadDuration} from "@/features/post/utils";
-import {Post as PostType} from "@/services/api/gen";
-import {user} from "@/pages/post/data/user";
+import {Post as PostType, User} from "@/services/api/gen";
+import {UserProvider} from "@/services/api";
 
 import blankUserProfile from "@/assets/noun-user-picture.svg";
 
@@ -13,129 +13,142 @@ export interface PostProps {
   post: PostType;
 }
 
-export const Post: FC<PostProps> = ({post}: PostProps) => (
-  <Layout>
-    <div
-      data-testid="post-title"
-      className="mb-4 mt-40 flex w-full justify-center p-11 md:mt-20"
-    >
-      <p className="font-optical-sizing-auto normal font-title text-6xl font-bold">
-        {post?.title}
-      </p>
-    </div>
-    <div className="flex w-full justify-center">
-      <div data-testid="post-details" className="grid w-[31.25rem] grid-cols-3">
-        <div className="flex items-center justify-center">
-          <Icon icon="material-symbols-light:face-6" className="text-2xl" />
-          <span className="mx-1">
-            by <strong>{user?.first_name}</strong>
-          </span>
-        </div>
-        <div className="flex items-center justify-center">
-          <Icon
-            icon="material-symbols-light:nest-clock-farsight-analog-outline"
-            className="text-2xl"
-          />
-          {/* TODO: relative datetime (like: 1 week ago) for later */}
-          <span className="mx-1">
-            {calculateReadDuration(post?.content).minutes} min read
-          </span>
-        </div>
-        <div className="flex items-center justify-center">
-          <Icon
-            icon="material-symbols-light:calendar-month-outline"
-            className="text-2xl"
-          />
-          <span className="mx-1">
-            {post?.creation_datetime?.toLocaleDateString()}
-          </span>
+export const Post: FC<PostProps> = ({post}: PostProps) => {
+  const [postAuthor, setPostAuthor] = useState<User>({});
+
+  useEffect(() => {
+    UserProvider.getById(post?.author_id!).then((author) =>
+      setPostAuthor(author)
+    );
+  }, []);
+
+  return (
+    <Layout>
+      <div
+        data-testid="post-title"
+        className="mb-4 mt-40 flex w-full justify-center p-11 md:mt-20"
+      >
+        <p className="font-optical-sizing-auto normal font-title text-6xl font-bold">
+          {post?.title}
+        </p>
+      </div>
+      <div className="flex w-full justify-center">
+        <div
+          data-testid="post-details"
+          className="grid w-[31.25rem] grid-cols-3"
+        >
+          <div className="flex items-center justify-center">
+            <Icon icon="material-symbols-light:face-6" className="text-2xl" />
+            <span className="mx-1">
+              by <strong>{postAuthor?.first_name}</strong>
+            </span>
+          </div>
+          <div className="flex items-center justify-center">
+            <Icon
+              icon="material-symbols-light:nest-clock-farsight-analog-outline"
+              className="text-2xl"
+            />
+            {/* TODO: relative datetime (like: 1 week ago) for later */}
+            <span className="mx-1">
+              {calculateReadDuration(post?.content).minutes} min read
+            </span>
+          </div>
+          <div className="flex items-center justify-center">
+            <Icon
+              icon="material-symbols-light:calendar-month-outline"
+              className="text-2xl"
+            />
+            <span className="mx-1">
+              {post?.creation_datetime?.toLocaleDateString()}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="my-5 grid grid-cols-8">
-      <div className="col-span-1 h-[18.75rem]"></div>
-      <div className="col-span-6 grid grid-cols-8 gap-5">
-        {post.thumbnail_url && (
-          <div data-testid="post-banner" className="col-span-8 h-[35rem]">
-            <img
-              src={post.thumbnail_url}
-              className="rounded-sd h-full w-full object-cover"
-            />
+      <div className="my-5 grid grid-cols-8">
+        <div className="col-span-1 h-[18.75rem]"></div>
+        <div className="col-span-6 grid grid-cols-8 gap-5">
+          {post.thumbnail_url && (
+            <div data-testid="post-banner" className="col-span-8 h-[35rem]">
+              <img
+                src={post.thumbnail_url}
+                className="rounded-sd h-full w-full object-cover"
+              />
+            </div>
+          )}
+          <div className="col-span-2 border-r border-slate-200 p-4">
+            <div
+              data-testid="post-content-menu"
+              className="mx-4 flex flex-col justify-center"
+            >
+              <p className="mb-5 text-left font-title text-2xl">Menu</p>
+              <ul className="ml-1 text-left">
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+                <li>menu menu menu menu</li>
+              </ul>
+            </div>
           </div>
-        )}
-        <div className="col-span-2 border-r border-slate-200 p-4">
-          <div
-            data-testid="post-content-menu"
-            className="mx-4 flex flex-col justify-center"
-          >
-            <p className="mb-5 text-left font-title text-2xl">Menu</p>
-            <ul className="ml-1 text-left">
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-              <li>menu menu menu menu</li>
-            </ul>
-          </div>
-        </div>
-        <div data-testid="post-content" className="col-span-6 p-4">
-          <div className="mx-10">{post?.content}</div>
-          <div data-testid="post-tags" className="mx-10 flex w-full py-10">
-            <span className="mr-2">Tags : </span>
-            <div className="flex justify-evenly">
-              <Badge className="mx-1">Lorem</Badge>
-              <Badge className="mx-1">Ipsum</Badge>
-              <Badge className="mx-1">Hello</Badge>
+          <div data-testid="post-content" className="col-span-6 p-4">
+            <div className="mx-10">{post?.content}</div>
+            <div data-testid="post-tags" className="mx-10 flex w-full py-10">
+              <span className="mr-2">Tags : </span>
+              <div className="flex justify-evenly">
+                <Badge className="mx-1">Lorem</Badge>
+                <Badge className="mx-1">Ipsum</Badge>
+                <Badge className="mx-1">Hello</Badge>
+              </div>
             </div>
           </div>
         </div>
+        <div className="col-span-1 h-[18.75rem]"></div>
       </div>
-      <div className="col-span-1 h-[18.75rem]"></div>
-    </div>
-    <div data-testid="user-details" className="grid grid-cols-8 bg-slate-50 ">
-      <div className="col-span-1 h-[18.75rem]"></div>
-      <div className="col-span-6 grid h-[18.75rem] grid-cols-8">
-        <div
-          data-testid="user-profile-picture"
-          className=" col-span-2 flex justify-center"
-        >
-          <img
-            src={blankUserProfile}
-            className="m-auto h-[12.5rem] w-[12.5rem] rounded-md bg-white object-cover"
-          />
-        </div>
-        <div className="container col-span-6 flex flex-col justify-center">
-          <Link
-            to=""
-            className="mb-5 text-left font-title text-2xl hover:text-slate-700 focus:text-slate-200 active:font-semibold"
+      <div data-testid="user-details" className="grid grid-cols-8 bg-slate-50 ">
+        <div className="col-span-1 h-[18.75rem]"></div>
+        <div className="col-span-6 grid h-[18.75rem] grid-cols-8">
+          <div
+            data-testid="user-profile-picture"
+            className=" col-span-2 flex justify-center"
           >
-            {user?.first_name} {user?.last_name}
-          </Link>
-          <p className="overflow-hidden truncate">{user?.about}</p>
-          <Link
-            to=""
-            className=" my-3 underline hover:text-slate-700 focus:text-slate-700 active:font-semibold"
-          >
-            See more about this author
-          </Link>
+            <img
+              src={blankUserProfile}
+              className="m-auto h-[12.5rem] w-[12.5rem] rounded-md bg-white object-cover"
+            />
+          </div>
+          <div className="container col-span-6 flex flex-col justify-center">
+            <Link
+              to=""
+              className="mb-5 text-left font-title text-2xl hover:text-slate-700 focus:text-slate-200 active:font-semibold"
+            >
+              {postAuthor?.first_name} {postAuthor?.last_name}
+            </Link>
+            <p className="overflow-hidden truncate">{postAuthor?.about}</p>
+            <Link
+              to=""
+              className=" my-3 underline hover:text-slate-700 focus:text-slate-700 active:font-semibold"
+            >
+              See more about this author
+            </Link>
+          </div>
         </div>
+        <div className="col-span-1 h-[18.75rem]"></div>
       </div>
-      <div className="col-span-1 h-[18.75rem]"></div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
