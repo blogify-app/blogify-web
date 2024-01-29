af826158fca2959db977f51f2611017f8a0a70ee
NyH1409 <hei.nyhasina@gmail.com>
2024-01-29 14:01:58 +0300

    feat: post card

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {DummyAuthenticatedPage, LoginPage, SignupPage} from "@/pages/auth";
 import {PostPage, WritePostPage} from "@/pages/post";
 import {Authenticated} from "@/features/auth";
 import "./index.css";
-import {Profile} from "./pages/profile";
+import {ProfilePage} from "@/pages/profile";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const ROUTER = createBrowserRouter([
@@ -40,7 +40,7 @@ const ROUTER = createBrowserRouter([
   },
   {
     path: "/users/:id",
-    element: <Profile />,
+    element: <ProfilePage />,
   },
 ]);
 
