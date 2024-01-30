83ab9e7d48ed3ecfa851fe3b7e02eee5c26d36e2
Yume <hei.hajatiana@gmail.com>
2024-01-30 14:53:49 +0300

    chore: pass accessToken to gen

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -6,19 +6,20 @@ import {
   HealthApi,
   SecurityApi,
 } from "@/services/api/gen";
+import {AuthProvider} from "@/services/security";
 
 // TODO: pass auth_config
 
-export const userApi = () => new UserApi();
+export const userApi = () => new UserApi(AuthProvider.getAuthConf());
 
-export const postingApi = () => new PostingApi();
+export const postingApi = () => new PostingApi(AuthProvider.getAuthConf());
 
-export const followingApi = () => new FollowingApi();
+export const followingApi = () => new FollowingApi(AuthProvider.getAuthConf());
 
-export const commentApi = () => new CommentsApi();
+export const commentApi = () => new CommentsApi(AuthProvider.getAuthConf());
 
 export const healthApi = () => new HealthApi();
 
-export const securityApi = () => new SecurityApi();
+export const securityApi = () => new SecurityApi(AuthProvider.getAuthConf());
 
 export * from "./provider";
