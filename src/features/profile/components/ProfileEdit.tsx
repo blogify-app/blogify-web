c28472e0722d281090b1a7a120b702cedddeb030
Dinasoa <hei.dinasoa@gmail.com>
2024-01-30 09:48:07 +0300

    feat: implement profile edit form

--- src/features/profile/components/ProfileEdit.tsx
+++ src/features/profile/components/ProfileEdit.tsx
@@ -14,6 +14,7 @@ import {
   FormMessage,
 } from "@/components/shadcn-ui/form.tsx";
 import {Calendar} from "@/components/shadcn-ui/calendar.tsx";
+import {Label} from "@/components/shadcn-ui/label";
 import {Textarea} from "@/components/shadcn-ui/textarea.tsx";
 import {
   Popover,
@@ -42,184 +43,191 @@ export const ProfileEdit: FC<ProfileEditProps> = () => {
 
   return (
     <Form {...form}>
-      <div className="bg-black-400 mb-6 flex w-[40rem] flex-col items-center justify-center space-y-6">
-        <div className="w-full">
-          <FormField
-            name="first_name"
-            control={form.control}
-            render={({field}) => (
-              <FormItem className="text-md">
-                <FormLabel>First name</FormLabel>
-                <FormControl className="h-12">
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+      <div className="flex h-full w-full flex-row">
+        <div className="mb-6 mt-6 flex w-1/2 w-[40rem] flex-col items-center justify-center space-y-2">
+          <div className="w-4/5">
+            <FormField
+              name="first_name"
+              control={form.control}
+              render={({field}) => (
+                <FormItem className="text-md">
+                  <FormLabel>First name</FormLabel>
+                  <FormControl className="h-12">
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="w-4/5">
+            <FormField
+              name="last_name"
+              control={form.control}
+              render={({field}) => (
+                <FormItem className="text-md">
+                  <FormLabel>Last name</FormLabel>
+                  <FormControl className="h-12">
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <div className="w-full">
-          <FormField
-            name="last_name"
-            control={form.control}
-            render={({field}) => (
-              <FormItem className="text-md">
-                <FormLabel>Last name</FormLabel>
-                <FormControl className="h-12">
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+          <div className="w-4/5">
+            <FormField
+              name="username"
+              control={form.control}
+              render={({field}) => (
+                <FormItem className="text-md">
+                  <FormLabel>User name</FormLabel>
+                  <FormControl className="h-12">
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <div className="w-full">
-          <FormField
-            name="username"
-            control={form.control}
-            render={({field}) => (
-              <FormItem className="text-md">
-                <FormLabel>User name</FormLabel>
-                <FormControl className="h-12">
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+          <div className="w-4/5">
+            <FormField
+              control={form.control}
+              name="sex"
+              render={({field}) => (
+                <FormItem className="space-y-3">
+                  <FormLabel>Sex</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="M" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Male</FormLabel>
+                      </FormItem>
 
-        <div className="w-full">
-          <FormField
-            control={form.control}
-            name="sex"
-            render={({field}) => (
-              <FormItem className="space-y-3">
-                <FormLabel>Sex</FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                    className="flex flex-col space-y-1"
-                  >
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="M" />
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="F" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Female</FormLabel>
+                      </FormItem>
+
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="OTHER" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Prefer not to say
+                        </FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="w-4/5">
+            <FormField
+              name="birth_date"
+              control={form.control}
+              render={({field}) => (
+                <FormItem className="text-md flex flex-col">
+                  <FormLabel>Date of birth</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl className="w-full">
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
                       </FormControl>
-                      <FormLabel className="font-normal">Male</FormLabel>
-                    </FormItem>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormDescription>
+                    Your date of birth is used to calculate your age.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="F" />
-                      </FormControl>
-                      <FormLabel className="font-normal">Female</FormLabel>
-                    </FormItem>
+          <div className="w-4/5">
+            <FormField
+              name="bio"
+              control={form.control}
+              render={({field}) => (
+                <FormItem className="text-md">
+                  <FormLabel>Bio</FormLabel>
+                  <FormControl className="h-12">
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="OTHER" />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        Prefer not to say
-                      </FormLabel>
-                    </FormItem>
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <div className="h-auto w-4/5">
+            <FormField
+              name="about"
+              control={form.control}
+              render={({field}) => (
+                <FormItem className="text-md">
+                  <FormLabel>About</FormLabel>
+                  <FormControl className="h-12">
+                    <Textarea {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="w-full">
+            <Button
+              className="h-12 w-full rounded-full"
+              // onClick={form.handleSubmit(onCreate)}
+            >
+              Continue
+            </Button>
+          </div>
         </div>
-
-        <div className="w-full">
-          <FormField
-            name="birth_date"
-            control={form.control}
-            render={({field}) => (
-              <FormItem className="text-md flex flex-col">
-                <FormLabel>Date of birth</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl className="w-full">
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-full pl-3 text-left font-normal",
-                          !field.value && "text-muted-foreground"
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      disabled={(date) =>
-                        date > new Date() || date < new Date("1900-01-01")
-                      }
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormDescription>
-                  Your date of birth is used to calculate your age.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-
-        <div className="w-full">
-          <FormField
-            name="bio"
-            control={form.control}
-            render={({field}) => (
-              <FormItem className="text-md">
-                <FormLabel>Bio</FormLabel>
-                <FormControl className="h-12">
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-
-        <div className="h-auto w-full">
-          <FormField
-            name="about"
-            control={form.control}
-            render={({field}) => (
-              <FormItem className="text-md">
-                <FormLabel>About</FormLabel>
-                <FormControl className="h-12">
-                  <Textarea {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-
-        <div className="w-full">
-          <Button
-            className="h-12 w-full rounded-full"
-            // onClick={form.handleSubmit(onCreate)}
-          >
-            Continue
-          </Button>
+        <div className="mb-6 mt-6 flex h-2/3 w-1/2 w-[40rem] flex-col items-center justify-center space-y-2">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="picture">Profile picture</Label>
+            <Input id="picture" type="file" />
+          </div>
         </div>
       </div>
     </Form>
