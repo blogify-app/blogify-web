a9c066f955422a033bc2527f9ccf0390619d92b8
Yume <hei.hajatiana@gmail.com>
2024-01-30 11:17:10 +0300

    feat: add useLoading hook

---
+++ src/hooks/use_loading.ts
@@ -0,0 +1,28 @@
+import {useCallback, useState} from "react";
+
+type QueueTask = <T>(cb: () => Promise<T>) => Promise<T>;
+
+export const useLoading = (id: string) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const queue: QueueTask = useCallback(
+    async (fn) => {
+      setIsLoading(true);
+      try {
+        const ret = await fn();
+        setIsLoading(false);
+        return ret;
+      } catch (e) {
+        setIsLoading(false);
+        throw e;
+      }
+    },
+    [setIsLoading]
+  );
+
+  return {
+    id,
+    queue,
+    isLoading,
+  };
+};
