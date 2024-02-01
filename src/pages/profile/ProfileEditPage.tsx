4282f1ca3f4976786532025bb95da85f9d9e23da
lianafinaritraratovo <hei.finaritra@gmail.com>
2024-02-01 16:39:33 +0300

    feat: implement profile edit page

---
+++ src/pages/profile/ProfileEditPage.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import {Layout, AnonymousHeader} from "@/layout";
+// import {ProfileEdit} from "@/features/profile";
+
+export const ProfileEditPage: FC = () => {
+  return (
+    <Layout header={<AnonymousHeader page="sign_up" />}>
+      <div className="mx-[2.8rem] h-full pt-[3.8rem] md:mx-[11rem]"></div>
+    </Layout>
+  );
+};
