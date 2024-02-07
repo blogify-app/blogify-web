5ff2106e1b1f463595f2e9610291304ff62b4fc8
Judicael <judi.randriamanjato@gmail.com>
2024-02-07 19:10:37 +0300

    wip: implement test

--- cypress/e2e/post_list.cy.tsx
+++ cypress/e2e/post_list.cy.tsx
@@ -1,10 +1,28 @@
-import {createPosts} from "../fixtures/post.ts";
+import {createPosts, post1} from "../fixtures/post.ts";
 
 describe("PostList", () => {
-  it("should redirect to /posts when landing on /", () => {
+  it("should render all posts", () => {
     cy.visit("/posts");
 
     // "*" to match any value of 'page' and 'page_size'
-    cy.intercept("GET", `**/posts?page=*&page_size=*`, createPosts(1));
+    cy.intercept("GET", `**/posts?page=*&page_size=*`, createPosts(10));
+    cy.intercept("GET", `**/posts/${post1().id}`, post1());
   });
+
+  it("should show one post when a specific post is clicked on", () => {
+    cy.visit("/posts");
+
+    // "*" to match any value of 'page' and 'page_size'
+    cy.intercept("GET", `**/posts?page=*&page_size=*`, createPosts(10));
+    cy.intercept("GET", `**/posts/${post1().id}`, post1());
+
+    cy.getByTestid('post_1').click();
+    cy.window()
+        .its("location")
+        .should((location) => {
+          expect(location.pathname).to.eq("/posts/post_1");
+        });
+
+  });
+
 });
